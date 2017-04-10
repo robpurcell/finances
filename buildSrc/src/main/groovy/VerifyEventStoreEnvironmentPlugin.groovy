77a4fc90d1cc4c62322244508fe1fcf1e60828f4
Rob Purcell <rob.purcell@gmail.com>
2017-04-10 07:11:27 +0100

    Minor formatting

--- buildSrc/src/main/groovy/VerifyEventStoreEnvironmentPlugin.groovy
+++ buildSrc/src/main/groovy/VerifyEventStoreEnvironmentPlugin.groovy
@@ -1,15 +1,17 @@
-import org.gradle.api.*
-
+import org.gradle.api.Plugin
+import org.gradle.api.Project
 
 class VerifyEventStoreEnvironmentPlugin implements Plugin<Project> {
-    void apply(Project project) {
-       project.test {
-            beforeSuite { x ->
-                if (x.parent == null) {
-                    if (System.getenv("EVENTUATE_API_KEY_ID") == null && System.getenv("EVENTUATE_API_KEY_SECRET") == null)
-                        logger.warn("\nPLEASE make sure that Eventuate-related environment variables EVENTUATE_API_KEY_ID and EVENTUATE_API_KEY_SECRET are set, see sample-set-remote-env.sh !!!!\n")
-                }
-            }
+
+  void apply(Project project) {
+    project.test {
+      beforeSuite { x ->
+        if (x.parent == null) {
+          if (System.getenv("EVENTUATE_API_KEY_ID") == null && System.getenv("EVENTUATE_API_KEY_SECRET") == null)
+            logger.warn("\nPLEASE make sure that Eventuate-related environment variables EVENTUATE_API_KEY_ID and " +
+                          "EVENTUATE_API_KEY_SECRET are set, see sample-set-remote-env.sh !!!!\n")
         }
+      }
     }
+  }
 }
